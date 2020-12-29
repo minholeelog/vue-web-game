746f962df2bee63edd96ff825819d48c37ba2367
91aidenlee <91aiden.lee@gmail.com>
2020-12-30 00:54:35 +0900

    웹팩 설정

---
+++ 3.숫자야구/NumberBaseball.vue
@@ -0,0 +1,28 @@
+<template>
+   <div>
+       <h1>{{ result }}</h1>
+       <form v-on:submit="onSubmitForm">
+           <input ref="answer" v-model="value" type="text" maxlength="4">
+           <button>입력</button>
+       </form>
+       <div>시도 : {{  }}</div>
+   </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            value: "",
+            result: "",
+        }
+    },
+    methods: {
+        onSubmitForm(evt) {
+            e.preventDefault();
+        }
+    },
+}
+</script>
+<style>
+    
+</style>
