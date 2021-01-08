2b694ecf757577bfcfd642f2d2ba0b8d8ea049dc
91aidenlee <91aiden.lee@gmail.com>
2021-01-08 23:26:45 +0900

    Apply gitignore

---
+++ 4.반응속도체크/ResponseCheck.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div id="screen">{{}}</div>
+    <div>평균 시간: {{}}</div>
+    <button @click="onReset">리셋</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      result: [],
+      state: "waiting",
+      message: "클릭해서 시작하세요",
+    };
+  },
+  methods: {
+    onReset() {},
+  },
+};
+</script>
+<style></style>
